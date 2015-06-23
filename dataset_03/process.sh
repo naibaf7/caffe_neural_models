daadc2a9bcdd1851f0a7677faa83d1fe6bc2c48b
Fabian Tschopp <fabian@m-tschopp.ch>
2015-06-23 03:58:46 +0200

    Model updates.

---
+++ dataset_03/process.sh
@@ -0,0 +1,1 @@
+./../../caffe_neural_tool/build/caffe_neural_tool --gpu 0 --graphic --process 0 --proto 'train_process_u_2.prototxt'
---
+++ dataset_03/train.sh
@@ -0,0 +1,1 @@
+./../../caffe_neural_tool/build/caffe_neural_tool --gpu 3 --train 0 --graphic --proto 'train_process_u_2.prototxt'
