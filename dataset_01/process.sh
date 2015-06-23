64cd788b80dfbc5b8ae21cc352d1da614c0c49a4
Fabian Tschopp <fabian@m-tschopp.ch>
2015-06-23 15:55:23 +0200

    Model and learning rate updates.

--- dataset_01/process.sh
+++ dataset_01/process.sh
@@ -1,1 +1,1 @@
-./../../caffe_neural_tool/build/caffe_neural_tool --gpu 0 --graphic --process 0 --proto 'train_process_sk_9.prototxt'
+./../../caffe_neural_tool/build/caffe_neural_tool --gpu 3 --process 0 --proto 'train_process_usk_9.prototxt'
