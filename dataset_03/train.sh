3143ee64322bb3d6701317d28791c9e7f3f94638
fabian <naibaf710@hotmail.com>
2015-07-01 16:04:18 -0400

    Malis loss layer training.

--- dataset_03/train.sh
+++ dataset_03/train.sh
@@ -1,1 +1,1 @@
-./../../caffe_neural_tool/build/caffe_neural_tool --gpu 3 --train 0 --graphic --proto 'train_process_u_2.prototxt'
+gdb --args ./../../caffe_neural_tool/build/caffe_neural_tool_dbg --gpu 0 --train 0 --graphic --proto 'train_process_usk_2.prototxt'
