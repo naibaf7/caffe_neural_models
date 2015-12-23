409c56e198e3cb743596ecf0358013e5f73b0d8c
fabian <naibaf710@hotmail.com>
2015-12-23 01:22:37 +0100

    Merge.

--- pygt_snemi_aniso/train.py
+++ pygt_snemi_aniso/train.py
@@ -17,62 +17,6 @@ import math
 # Load PyGreentea
 import PyGreentea as pygt
 
-# Create the network we want
-class NetConf:
-    # 10 GB total memory limit
-    mem_global_limit = 10 * 1024 * 1024 * 1024
-    # 4 GB single buffer memory limit
-    mem_buf_limit = 4 * 1024 * 1024 * 1024
-    # Desired input dimensions (will select closest possible)
-    input_shape = [44,132,132]
-    # Desired output dimensions (will select closest posisble)
-    output_shape = [16, 44, 44]
-    # Number of U-Net Pooling-Convolution downsampling/upsampling steps
-    unet_depth = 3
-    # Number of feature maps in the start
-    fmap_start = 64
-    # Number of input feature maps
-    fmap_input = 1
-    # Number of ouput feature maps
-    fmap_output = 11
-    # Feature map increase rule (downsampling)
-    def unet_fmap_inc_rule(self, fmaps):
-        return int(math.ceil(fmaps * 3));
-    # Feature map decrease rule (upsampling)
-    def unet_fmap_dec_rule(self, fmaps):
-        return int(math.ceil(fmaps / 3));
-    # Skewed U-Net downsampling strategy
-    unet_downsampling_strategy = [[1,2,2],[1,2,2],[1,2,2]]
-    # Number of SK-Net Pooling-Convolution steps
-    sknet_conv_depth = 0
-    # Feature map increase rule
-    def sknet_fmap_inc_rule(self, fmaps):
-        return int(math.ceil(fmaps * 1.5));
-    # Number of 1x1 (IP) Convolution steps
-    sknet_ip_depth = 0
-    # Feature map increase rule from SK-Convolution to IP
-    def sknet_fmap_bridge_rule(self, fmaps):
-        return int(math.ceil(fmaps * 4));
-    # Feature map decrease rule within IP
-    def sknet_fmap_dec_rule(self, fmaps):
-        return int(math.ceil(fmaps / 2.5));
-    # Loss function and mode ("malis", "euclid", "softmax")
-    loss_function = "euclid"
-
-
-netconf = NetConf()
-netconf.loss_function = "euclid"
-train_net_conf_euclid, test_net_conf = pygt.netgen.create_nets(netconf)
-netconf.loss_function = "malis"
-train_net_conf_malis, test_net_conf = pygt.netgen.create_nets(netconf)
-
-with open('net_train_euclid.prototxt', 'w') as f:
-    print(train_net_conf_euclid, file=f)
-with open('net_train_malis.prototxt', 'w') as f:
-    print(train_net_conf_malis, file=f)
-with open('net_test.prototxt', 'w') as f:
-    print(test_net_conf, file=f)
-
 # Load the datasets
 hdf5_raw_file = '/groups/turaga/home/turagas/data/SNEMI3D/train/raw.hdf5'
 hdf5_gt_file = '/groups/turaga/home/turagas/data/SNEMI3D/train/labels_id.hdf5'
@@ -104,7 +48,7 @@ class TrainOptions:
     test_interval = 4000
     scale_error = True
     training_method = "affinity"
-    recompute_affinity = True
+    recompute_affinity = False
     train_device = 0
     test_device = 2
     test_net='net_test.prototxt'
@@ -115,7 +59,7 @@ options = TrainOptions()
 # Set solver options
 solver_config = pygt.caffe.SolverParameter()
 solver_config.train_net = 'net_train_euclid.prototxt'
-solver_config.base_lr = 0.00005
+solver_config.base_lr = 0.001
 solver_config.momentum = 0.99
 solver_config.weight_decay = 0.000005
 solver_config.lr_policy = 'inv'
