960040cd101cdf37f8988e3c52a8bc4c997f7dcc
fabian <naibaf710@hotmail.com>
2016-01-14 21:22:08 +0100

    New examples

--- pygt_fibsem/script.py
+++ pygt_fibsem/script.py
@@ -17,51 +17,15 @@ import math
 # Load PyGreentea
 import PyGreentea as pygt
 
-# Create the network we want
-class NetConf:
-    # 10 GB total memory limit
-    mem_global_limit = 10 * 1024 * 1024 * 1024
-    # 4 GB single buffer memory limit
-    mem_buf_limit = 4 * 1024 * 1024 * 1024
-    # Desired input dimensions (will select closest possible)
-    input_shape = [132,132,132]
-    # Desired output dimensions (will select closest posisble)
-    output_shape = [44, 44, 44]
-    # Number of U-Net Pooling-Convolution downsampling/upsampling steps
-    unet_depth = 3
-    # Number of feature maps in the start
-    fmap_start = 16
-    # Number of input feature maps
-    fmap_input = 1
-    # Number of ouput feature maps
-    fmap_output = 3
-    # Feature map increase rule (downsampling)
-    def unet_fmap_inc_rule(self, fmaps):
-        return int(math.ceil(fmaps * 3));
-    # Feature map decrease rule (upsampling)
-    def unet_fmap_dec_rule(self, fmaps):
-        return int(math.ceil(fmaps / 3));
-    # Skewed U-Net downsampling strategy
-    unet_downsampling_strategy = [[2,2,2],[2,2,2],[2,2,2]]
-    # Number of SK-Net Pooling-Convolution steps
-    sknet_conv_depth = 0
-    # Feature map increase rule
-    def sknet_fmap_inc_rule(self, fmaps):
-        return int(math.ceil(fmaps * 1.5));
-    # Number of 1x1 (IP) Convolution steps
-    sknet_ip_depth = 0
-    # Feature map increase rule from SK-Convolution to IP
-    def sknet_fmap_bridge_rule(self, fmaps):
-        return int(math.ceil(fmaps * 4));
-    # Feature map decrease rule within IP
-    def sknet_fmap_dec_rule(self, fmaps):
-        return int(math.ceil(fmaps / 2.5));
-    # Loss function and mode ("malis", "euclid", "softmax")
-    loss_function = "euclid"
 
 
-netconf = NetConf()
+netconf = pygt.netgen.NetConf()
+
+
 netconf.loss_function = "euclid"
+print(pygt.netgen.compute_valid_io_shapes(netconf,pygt.netgen.caffe_pb2.TRAIN,[30,30,30],[500,500,500],constraints=[None,lambda x: x[0], lambda x: x[1]]))
+
+
 train_net_conf_euclid, test_net_conf = pygt.netgen.create_nets(netconf)
 netconf.loss_function = "malis"
 train_net_conf_malis, test_net_conf = pygt.netgen.create_nets(netconf)
