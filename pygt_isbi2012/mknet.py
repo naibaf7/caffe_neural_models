f34ede625451966578860f8224b753a5fad4af28
fabian <naibaf710@hotmail.com>
2016-01-28 19:07:06 +0100

    new examples

---
+++ pygt_isbi2012/mknet.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+import h5py
+import numpy as np
+from numpy import float32, int32, uint8, dtype
+import sys
+
+# Relative path to where PyGreentea resides
+pygt_path = '../../PyGreentea'
+
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), pygt_path))
+
+# Other python modules
+import math
+
+# Load PyGreentea
+import PyGreentea as pygt
+
+
+netconf = pygt.netgen.NetConf()
+
+netconf.use_batchnorm = False
+netconf.dropout = 0.0
+
+netconf.fmap_start = 4
+
+#sk1 = pygt.netgen.SKNetConf()
+#sk1.sknet_conv = [[8],[6],[4]]
+
+#sk2 = pygt.netgen.SKNetConf()
+#sk2.sknet_padding = [44,44]
+#sk2.sknet_conv = [[6],[4],[2]]
+
+netconf.u_netconfs[0].unet_depth = 3
+netconf.u_netconfs[0].unet_fmap_inc_rule = lambda fmaps: int(math.ceil(float(fmaps) * 1))
+netconf.u_netconfs[0].unet_fmap_dec_rule = lambda fmaps: int(math.ceil(float(fmaps) / 1))
+# netconf.u_netconfs[0].sk_netconfs = [None,sk1,sk2]
+# netconf.u_netconfs = [netconf.u_netconfs[0],netconf.u_netconfs[0]]
+
+
+netconf.loss_function = "euclid"
+inshape,outshape,fmaps = pygt.netgen.compute_valid_io_shapes(netconf,pygt.netgen.caffe_pb2.TRAIN,[30,30],[200,200],constraints=[None,lambda x: x[0], lambda x: x[1]])
+
+
+# We choose the maximum that still gives us 20 fmaps:
+index = [n for n, i in enumerate(fmaps) if i>=4][-1]
+print("Index to use: %s" % index)
+# Some patching to allow our new parameters
+netconf.input_shape = inshape[index]
+netconf.output_shape = outshape[index]
+netconf.fmap_start = 4
+
+netconf.loss_function = "euclid"
+train_net_conf_euclid, test_net_conf = pygt.netgen.create_nets(netconf)
+netconf.loss_function = "malis"
+train_net_conf_malis, test_net_conf = pygt.netgen.create_nets(netconf)
+
+with open('net_train_euclid.prototxt', 'w') as f:
+    print(train_net_conf_euclid, file=f)
+with open('net_train_malis.prototxt', 'w') as f:
+    print(train_net_conf_malis, file=f)
+with open('net_test.prototxt', 'w') as f:
+    print(test_net_conf, file=f)
+
---
+++ pygt_isbi2012/train.py
@@ -0,0 +1,95 @@
+from __future__ import print_function
+import h5py
+import numpy as np
+from numpy import float32, int32, uint8, dtype
+import sys
+
+# Relative path to where PyGreentea resides
+pygt_path = '../../PyGreentea'
+
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), pygt_path))
+
+# Other python modules
+import math
+
+# Load PyGreentea
+import PyGreentea as pygt
+
+# Load the datasets
+hdf5_raw_file = '../dataset_06/fibsem_medulla_7col/tstvol-520-1-h5/img_normalized.h5'
+hdf5_gt_file = '../dataset_06/fibsem_medulla_7col/tstvol-520-1-h5/groundtruth_seg.h5'
+hdf5_aff_file = '../dataset_06/fibsem_medulla_7col/tstvol-520-1-h5/groundtruth_aff.h5'
+
+hdf5_raw = h5py.File(hdf5_raw_file, 'r')
+hdf5_gt = h5py.File(hdf5_gt_file, 'r')
+hdf5_aff = h5py.File(hdf5_aff_file, 'r')
+
+hdf5_raw_ds = pygt.normalize(np.asarray(hdf5_raw[hdf5_raw.keys()[0]]).astype(float32), -1, 1)
+hdf5_gt_ds = np.asarray(hdf5_gt[hdf5_gt.keys()[0]]).astype(float32)
+hdf5_aff_ds = np.asarray(hdf5_aff[hdf5_aff.keys()[0]]).astype(float32)
+
+
+dataset = {}
+dataset['data'] = hdf5_raw_ds[None, :]
+dataset['components'] = hdf5_gt_ds[None, :]
+dataset['label'] = hdf5_aff_ds[:]
+dataset['nhood'] = pygt.malis.mknhood2d()
+
+#test_dataset = {}
+#test_dataset['data'] = hdf5_raw_ds
+#test_dataset['label'] = hdf5_aff_ds
+
+
+# Set train options
+class TrainOptions:
+    loss_function = "euclid"
+    loss_output_file = "log/loss.log"
+    test_output_file = "log/test.log"
+    test_interval = 4000
+    scale_error = True
+    training_method = "affinity"
+    recompute_affinity = True
+    train_device = 0
+    test_device = 0
+    test_net=None #'net_test.prototxt'
+
+
+options = TrainOptions()
+
+# Set solver options
+solver_config = pygt.caffe.SolverParameter()
+solver_config.train_net = 'net_train_euclid.prototxt'
+solver_config.base_lr = 0.00001
+solver_config.momentum = 0.99
+solver_config.weight_decay = 0.000005
+solver_config.lr_policy = 'inv'
+solver_config.gamma = 0.0001
+solver_config.power = 0.75
+solver_config.max_iter = 8000
+solver_config.snapshot = 2000
+solver_config.snapshot_prefix = 'net'
+solver_config.type = 'Adam'
+solver_config.display = 1
+
+# Set devices
+# pygt.caffe.enumerate_devices(False)
+pygt.caffe.set_devices((options.train_device,))
+
+
+solverstates = pygt.getSolverStates(solver_config.snapshot_prefix);
+
+# First training method
+if (len(solverstates) == 0 or solverstates[-1][0] < solver_config.max_iter):
+    solver, test_net = pygt.init_solver(solver_config, options)
+    if (len(solverstates) > 0):
+        solver.restore(solverstates[-1][1])
+    pygt.train(solver, test_net, [dataset], [], options)
+    
+
+
+
+
+
+
